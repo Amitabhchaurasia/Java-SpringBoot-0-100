a408e62a9b9d58d02c579ab390a4075787bb31d0
Amitabh <amitabhchaurasia8@gmail.com>
2024-07-12 10:15:17 +0530

    week 2 project setup

---
+++ week2/springwebtutorila/src/main/java/com/springwebtutorial/springwebtutorila/SpringwebtutorilaApplication.java
@@ -0,0 +1,13 @@
+package com.springwebtutorial.springwebtutorila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebtutorilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebtutorilaApplication.class, args);
+	}
+
+}
---
+++ week2/springwebtutorila/src/test/java/com/springwebtutorial/springwebtutorila/SpringwebtutorilaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springwebtutorial.springwebtutorila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebtutorilaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
