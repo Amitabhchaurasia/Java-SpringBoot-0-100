ffb5a0fe9361bd5bdd1e37be52a3b09d7514c880
Amitabh <amitabhchaurasia8@gmail.com>
2024-07-15 14:43:44 +0530

    worked on diff-diff req

--- week2/springwebtutorila/src/main/java/com/springwebtutorial/springwebtutorila/controller/EmployeeController.java
+++ week2/springwebtutorila/src/main/java/com/springwebtutorial/springwebtutorila/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.springwebtutorial.springwebtutorila.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.springwebtutorial.springwebtutorila.dto.EmployeeDTO;
 import com.springwebtutorial.springwebtutorila.entity.EmployeeEntity;
 import com.springwebtutorial.springwebtutorila.repository.EmployeeRepository;
@@ -10,9 +11,15 @@ import com.springwebtutorial.springwebtutorila.service.EmployeeService;
 import java.time.LocalDate;
 
 import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,36 +27,60 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
-    
-//    @GetMapping(path = "/getmsg")
-//    public String getmsg(){
-//     return "This is from msg";
-//    }
 
-// private final EmployeeRepository employeeRepository;
-// EmployeeController(EmployeeRepository employeeRepository){
-//     this.employeeRepository = employeeRepository;
-// }
+    // @GetMapping(path = "/getmsg")
+    // public String getmsg(){
+    // return "This is from msg";
+    // }
 
-private final EmployeeService employeeService;
-EmployeeController(EmployeeService employeeService){
-    this.employeeService = employeeService;
-}
+    // private final EmployeeRepository employeeRepository;
+    // EmployeeController(EmployeeRepository employeeRepository){
+    // this.employeeRepository = employeeRepository;
+    // }
 
-@GetMapping("/{id}")
-public EmployeeDTO getEmpById(@PathVariable long id){
-    return employeeService.getEmpById(id);
-}
+    private final EmployeeService employeeService;
 
-@GetMapping
-public List<EmployeeDTO> getAllEmp(@RequestParam(required = false) Integer age, @RequestParam(required = false) String sortBy){
-    return employeeService.getAllEmp(age, sortBy);
-}
+    EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
 
-@PostMapping
-public EmployeeDTO addEmp(@RequestBody EmployeeDTO EmployeeDTO){
-    return employeeService.addEmp(EmployeeDTO);
+    @GetMapping("/{id}")
+    public EmployeeDTO getEmpById(@PathVariable long id) {
+        return employeeService.getEmpById(id);
+    }
 
-}
+    @GetMapping
+    public List<EmployeeDTO> getAllEmp(@RequestParam(required = false) Integer age,
+            @RequestParam(required = false) String sortBy) {
+        return employeeService.getAllEmp(age, sortBy);
+    }
+
+    @PostMapping
+    public ResponseEntity<EmployeeDTO> addEmp(@RequestBody EmployeeDTO EmployeeDTO) {
+         EmployeeDTO emD= employeeService.addEmp(EmployeeDTO);
+         if (emD == null) {
+             return ResponseEntity.badRequest().build();
+            
+         }
+         return ResponseEntity.ok(emD);
+
+    }
+
+    @PutMapping("/{id}")
+    public EmployeeDTO updateEmp(@PathVariable long id, @RequestBody EmployeeDTO employeeDTO) {
+        return employeeService.updateEmp(id, employeeDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean deleteEmp(@PathVariable long id) {
+        return employeeService.deleteEmp(id);
+    }
+    @PatchMapping("/{id}")
+    public EmployeeDTO updateUser(@PathVariable Long id, @RequestBody Map<String, Object> updates) throws JsonMappingException {
+       
+            return employeeService.patchEmp(id, updates);
+        
+    }
+
     
 }
--- week2/springwebtutorila/src/main/java/com/springwebtutorial/springwebtutorila/service/EmployeeService.java
+++ week2/springwebtutorila/src/main/java/com/springwebtutorial/springwebtutorila/service/EmployeeService.java
@@ -1,19 +1,29 @@
 package com.springwebtutorial.springwebtutorila.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springwebtutorial.springwebtutorila.dto.EmployeeDTO;
 import com.springwebtutorial.springwebtutorila.entity.EmployeeEntity;
 import com.springwebtutorial.springwebtutorila.repository.EmployeeRepository;
 
 @Service
 public class EmployeeService {
+    @Autowired
+    private ObjectMapper objectMapper;
     private final EmployeeRepository employeeRepository;
-    private final ModelMapper modelMapper ;
+    private ModelMapper modelMapper = new ModelMapper();
 
-    EmployeeService(EmployeeRepository employeeRepository, ModelMapper modelMapper){ 
+    EmployeeService(EmployeeRepository employeeRepository, ModelMapper modelMapper) {
         this.employeeRepository = employeeRepository;
         this.modelMapper = modelMapper;
     }
@@ -23,16 +33,43 @@ public class EmployeeService {
         return modelMapper.map(ee, EmployeeDTO.class);
     }
 
-    
     public List<EmployeeDTO> getAllEmp(Integer age, String sortBy) {
         List<EmployeeEntity> ee = employeeRepository.findAll();
-        return ee.stream().map(entity->modelMapper.map(entity, EmployeeDTO.class)).toList();
+        return ee.stream().map(entity -> modelMapper.map(entity, EmployeeDTO.class)).toList();
     }
 
     public EmployeeDTO addEmp(EmployeeDTO employeeDTO) {
-        EmployeeEntity tosaveEE=modelMapper.map(employeeDTO, EmployeeEntity.class);
+        EmployeeEntity tosaveEE = modelMapper.map(employeeDTO, EmployeeEntity.class);
         employeeRepository.save(tosaveEE);
         return modelMapper.map(tosaveEE, EmployeeDTO.class);
+    }
+
+    public EmployeeDTO updateEmp(long id, EmployeeDTO employeeDTO) {
+        EmployeeEntity ee = modelMapper.map(employeeDTO, EmployeeEntity.class);
+        ee.setId(id);
+        EmployeeEntity updatedEE = employeeRepository.save(ee);
+        return modelMapper.map(updatedEE, EmployeeDTO.class);
+    }
+
+    public boolean deleteEmp(long id) {
+        boolean exits = employeeRepository.existsById(id);
+        if (exits) {
+            employeeRepository.deleteById(id);
+            return true;
+        }
+
+        return false;
+    }
+
+    public EmployeeDTO patchEmp(Long id, Map<String, Object> updates) throws JsonMappingException {
+        EmployeeEntity ee = employeeRepository.findById(id).orElse(null);
+        if (ee == null) {
+            return null;
+        }
+        objectMapper.updateValue(ee, updates);
+        EmployeeEntity updatedEE = employeeRepository.save(ee);
+        return modelMapper.map(updatedEE, EmployeeDTO.class);
+
     }
 
 }
